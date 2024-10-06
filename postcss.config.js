3058bc8a415e7531ad1c048d26ee23eef3b5b2a1
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2024-10-06 22:51:31 +0300

    Intial Design

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+import autoprefixer from 'autoprefixer';
+import postcssNesting from 'postcss-nesting';
+
+export default {
+    plugins: [
+        autoprefixer(),
+        postcssNesting()
+    ]
+}
